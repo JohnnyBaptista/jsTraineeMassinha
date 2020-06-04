27fe3fc0dd3dce16bca8c50c201323338ab8dc6a
JohnnyBaptista <joaosbaptista@hotmail.com>
2020-06-03 22:39:48 -0300

    deixando pronto para exercicio

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,9 @@ function start() {
 
     let multi = [];
     resultados = document.getElementsByClassName('result');
-
-    for (let i = 0; i < (resultados.length)/10 ; i++) {
-        for (let j = 0; j < 10; j++) {          
-            multi.push();
-        }
-    }
-
-    for (let i = 0; i < resultados.length; i++) {
-        resultados[i].innerText = multi[i];
-    }
     
+}
+
+function mult(a,b){
+    return a*b;
 }
